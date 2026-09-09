4bc062c50620408ec8352f59e8deaf96e8b1cefd
helo842 <helo842@vidalibarraquer.net>
2026-09-09 12:18:32 +0200

    correccion conflicto

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,10 +1,6 @@
 import { MovieController } from "../controllers/movieController.js";
 
-<<<<<<< HEAD
 const tituloBuscado = "dune"; // Simulación de input
-=======
-const tituloBuscado = "The Matrix"; // Simulación de input
->>>>>>> 9c67c6dc69d02d906686136ec45bc84d069a429b
 
 if (!window.__movieRequestRan) {
     window.__movieRequestRan = true;
